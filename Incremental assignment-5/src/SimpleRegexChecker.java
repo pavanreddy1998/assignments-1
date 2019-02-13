154043bd3df0e582cda8d0cefdae45c6986a13b9
pavanreddy21 <46338498+pavanreddy21@users.noreply.github.com>
2019-02-13 18:54:34 +0530

    Update SimpleRegexChecker.java

--- Incremental assignment-5/src/SimpleRegexChecker.java
+++ Incremental assignment-5/src/SimpleRegexChecker.java
@@ -9,7 +9,7 @@ public class SimpleRegexChecker {
         System.out.println("enter string");
         Scanner s = new Scanner(System.in);
         inputString=s.nextLine();
-        if(Pattern.matches("^[A-Z]..*$",inputString))
+        if(Pattern.matches("^[A-Z].*\\.$",inputString))
         System.out.println("String matched pattern");
         else System.out.println("String not matched with pattern");
     }
@@ -20,11 +20,11 @@ public class SimpleRegexChecker {
 /**
   *Test cases:
       Input                      Output
-    1.pavan                      String not matched with pattern
-    2.Pavan                      String matched pattern
-    3.my name is pavan reddy     String not matched with pattern
-    4.My name is pavan reddy     String matched pattern
-    5.heLLo WELCOME              String not matched with pattern
+    1.Pavan                      String not matched with pattern
+    2.Pavan.                     String matched pattern
+    3.my name is pavan reddy.    String not matched with pattern
+    4.My name is pavan reddy.     String matched pattern
+    5.heLLo WELCOME.             String not matched with pattern
     6.                           String not matched with pattern
     7.324234                     String not matched with pattern
 
